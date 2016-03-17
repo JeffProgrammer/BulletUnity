0ca2889e5a445b948c89cdba2280215c8f49de98
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-03-16 22:07:41 -0400

    initial basic start of the physics engine. Just a raw simulation loop.

--- src/physicsEngine.cpp
+++ src/physicsEngine.cpp
@@ -3,3 +3,41 @@
 // Copyright (c) 2014-2016 Jeff Hutchinson
 // All rights reserved.
 //------------------------------------------------------------------------------
+
+#include "physicsEngine.h"
+
+PhysicsEngine::PhysicsEngine() {
+	mAccumulator = 0.0;
+	mMaxSubSteps = 10;
+	mRunning = true;
+	mWorldGravity = btVector3(0.0f, -20.0f, 0.0f);
+
+	// create the physics dispatcher.
+	auto configuration = new btDefaultCollisionConfiguration();
+	mDispatcher = new btCollisionDispatcher(configuration);
+
+	// now we set up the world. The world takes the dispatcher, a braod phase
+	// interface setup, as well as a constraint solver.
+	mWorld = new btDiscreteDynamicsWorld(mDispatcher, new btDbvtBroadphase(), new btSequentialImpulseConstraintSolver(), configuration);
+	mWorld->setGravity(mWorldGravity);
+}
+
+PhysicsEngine::~PhysicsEngine() {
+	delete mWorld;
+	delete mDispatcher;
+}
+
+void PhysicsEngine::simulate(const double &dt) {
+	if (mRunning) {
+		mAccumulator += dt;
+		while (mAccumulator > PHYSICS_TICK) {
+			// Invoke callback to unity. This is where physics code
+			// that interfaces with bullet can be called.
+			// We will invoke it before simulating.
+
+			// Now, simulate the world so that the physics advances.
+			mWorld->stepSimulation(static_cast<btScalar>(PHYSICS_TICK), mMaxSubSteps);
+			mAccumulator -= PHYSICS_TICK;
+		}
+	}
+}
--- src/physicsEngine.h
+++ src/physicsEngine.h
@@ -3,3 +3,62 @@
 // Copyright (c) 2014-2016 Jeff Hutchinson
 // All rights reserved.
 //------------------------------------------------------------------------------
+
+#ifndef _BULLETPLUGIN_PHYSICSENGINE_H_
+#define _BULLETPLUGIN_PHYSICSENGINE_H_
+
+#include <btBulletDynamicsCommon.h>
+#include <BulletCollision/NarrowPhaseCollision/btManifoldPoint.h>
+#include <BulletCollision/CollisionDispatch/btInternalEdgeUtility.h>
+
+/**
+ * The fixed time step for the physics.
+ */
+#define PHYSICS_TICK 0.016666666
+
+class PhysicsEngine {
+public:
+	PhysicsEngine();
+	~PhysicsEngine();
+
+	/**
+	 * Simulates the physics engine.
+	 * @param dt The delta, in seconds, from the last frame to the current frame.
+	 */
+	void simulate(const double &dt);
+
+private:
+	/**
+	 * The world object that simulates the physics.
+	 */
+	btDiscreteDynamicsWorld *mWorld;
+
+	/**
+	 * Collision dispatcher for the physics world.
+	 */
+	btCollisionDispatcher *mDispatcher;
+
+	/**
+	 * Accumulator time. The physics has to run at a fixed update
+	 * at 60 times a second.
+	 */
+	double mAccumulator;
+
+	/**
+	 * Controls whether or not the physics engine is running.
+	 */
+	bool mRunning;
+
+	/**
+	 * Controls the maximum number of sub steps allowed in a simulation.
+	 * Default value is 10.
+	 */
+	int mMaxSubSteps;
+
+	/**
+	 * The world's gravity direction and strength.
+	 */
+	btVector3 mWorldGravity;
+};
+
+#endif // _BULLETPLUGIN_PHYSICSENGINE_H_
