d2bf6e366fa0be569ef025da19f003a7e1c1455f
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-03-16 22:21:45 -0400

    C linkage API for the physics engine

---
+++ src/physicsEngine_api.h
@@ -0,0 +1,67 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2014-2016 Glenn Smith
+// Copyright (c) 2014-2016 Jeff Hutchinson
+// All rights reserved.
+//------------------------------------------------------------------------------
+
+#ifndef _BULLETPLUGIN_PHYSICSENGINE_API_H_
+#define _BULLETPLUGIN_PHYSICSENGINE_API_H_
+
+#include "physicsEngine.h"
+
+extern "C" {
+	/**
+	 * Creates a new physics world.
+	 * @return a PhysicsWorld instance pointer.
+	 */
+	void* physics_engine_create() {
+		return new PhysicsEngine();
+	}
+
+	/**
+	 * Destroys a physics world.
+	 * @param [IN] physicsEngine The physics engine instance pointer.
+	 */
+	void physics_engine_destroy(void *physicsEngine) {
+		auto engine = static_cast<PhysicsEngine*>(physicsEngine);
+		delete engine;
+		engine = nullptr;
+	}
+
+	/**
+	 * Destroys a physics world.
+	 * @param [IN] physicsEngine The physics engine instance pointer.
+	 * @param [IN] dt The time delta in seconds from the last frame to the current
+	 *  frame.
+	 */
+	void physics_engine_simulate(void *physicsEngine, double dt) {
+		static_cast<PhysicsEngine*>(physicsEngine)->simulate(dt);
+	}
+
+	/**
+	 * Destroys a physics world.
+	 * @param [IN] physicsEngine The physics engine instance pointer.
+	 * @param [IN] gravity A 3-dimentional vector containing the direction and 
+	 *  magnitude of the world's default global gravity.
+	 */
+	void physics_engine_set_gravity(void *physicsEngine, float *gravity) {
+		static_cast<PhysicsEngine*>(physicsEngine)->setWorldGravity(btVector3(gravity[0], gravity[1], gravity[2]));
+	}
+
+
+	/**
+	 * Destroys a physics world.
+	 * @param [IN]  physicsEngine The physics engine instance pointer.
+	 * @param [OUT] gravity A 3-dimentional vector containing the direction and
+	 *  magnitude of the world's default global gravity.
+	 */
+	void physics_engine_get_gravity(void *physicsEngine, float *gravity) {
+		auto engine = static_cast<PhysicsEngine*>(physicsEngine);
+		const btVector3 &vec = engine->getWorldGravity();
+		gravity[0] = vec.x;
+		gravity[1] = vec.y;
+		gravity[2] = vec.z;
+	}
+}
+
+#endif // _BULLETPLUGIN_PHYSICSENGINE_API_H_
