527fb51aa0d40d1dfdc89af76e9624aff47195dd
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-04-05 23:52:21 -0400

    some docs on marshaling

--- src/marshal.h
+++ src/marshal.h
@@ -8,12 +8,22 @@
 #include <btBulletDynamicsCommon.h>
 
 namespace Marshal {
+	/**
+	 * Marshals a 3D vector from Bullet to a float array for Unity.
+	 * @param [IN]  vec The 3D vector input.
+	 * @param [OUT] arr The 3 length array, outputted, preallocated.
+	 */
 	inline void toArray(const btVector3 &vec, float *&arr) {
 		arr[0] = vec.x();
 		arr[1] = vec.y();
 		arr[2] = vec.z();
 	}
 
+	/**
+	 * Marshals a quaternion from Bullet to a float array for Unity.
+	 * @param [IN]  quat The quaternion input.
+	 * @param [OUT] arr  The 4 length array, outputted, preallocated.
+	 */
 	inline void toArray(const btQuaternion &quat, float *&arr) {
 		arr[0] = quat.x();
 		arr[1] = quat.y();
@@ -21,6 +31,10 @@ namespace Marshal {
 		arr[3] = quat.w();
 	}
 
+	/**
+	 * Marshals a 3 length float array to 
+	 *
+	 */
 	inline btVector3 toVector(const float *arr) {
 		return btVector3(arr[0], arr[1], arr[2]);
 	}
