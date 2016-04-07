4437d78de4057c06b9b0cc11a329eeb477ba0a38
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-04-07 13:33:13 -0400

    physics trigger API

--- src/physicsTrigger_api.cpp
+++ src/physicsTrigger_api.cpp
@@ -3,8 +3,33 @@
 //------------------------------------------------------------------------------
 
 #include "physicsTrigger_api.h"
+#include "physicsTrigger.h"
 #include "marshal.h"
 
 extern "C" {
-
+	void* physics_trigger_create(int triggerType) {
+		return new PhysicsTrigger(triggerType);
+	}
+	
+	void physics_trigger_destroy(void *trigger) {
+		PhysicsTrigger *trig = static_cast<PhysicsTrigger*>(trigger);
+		delete trig;
+		trig = nullptr;
+	}
+	
+	void physics_trigger_set_half_extents(void *trigger, float *halfExtents) {
+		static_cast<PhysicsTrigger*>(trigger)->setHalfExtents(Marshal::toVector(halfExtents));
+	}
+	
+	void physics_trigger_get_half_extents(void *trigger, float *halfExtents) {
+		Marshal::toArray(static_cast<PhysicsTrigger*>(trigger)->getExtents(), halfExtents);
+	}
+	
+	void physics_trigger_set_radius(void *trigger, float radius) {
+		static_cast<PhysicsTrigger*>(trigger)->setRadius(radius);
+	}
+	
+	float physics_trigger_get_radius(void *trigger) {
+		return static_cast<PhysicsTrigger*>(trigger)->getRadius();
+	}
 }
--- src/physicsTrigger_api.h
+++ src/physicsTrigger_api.h
@@ -5,4 +5,55 @@
 #ifndef _BULLETPLUGIN_PHYSICSTRIGGER_API_H_
 #define _BULLETPLUGIN_PHYSICSTRIGGER_API_H_
 
+#include "pluginAPI.h"
+
+extern "C" {
+	/**
+	 * Creates a trigger object with a collider shape specified.
+	 * @param [IN] triggerType The type of trigger we are going to create, such as
+	 *  box or sphere.
+	 * @return an instance pointer of a newly created physics trigger.
+	 */
+	PLUGIN_API void* physics_trigger_create(int triggerType);
+	
+	/**
+	 * Destorys the physics trigger instance.
+	 * @param [IN] trigger The physics trigger instance pointer.
+	 */
+	PLUGIN_API void physics_trigger_destroy(void *trigger);
+	
+	/**
+	 * Sets the extent of the physics trigger box shape.
+	 * @param [IN] trigger The physics trigger instance pointer.
+	 * @param [IN] halfExtents The size of the box's extents (3 floats).
+	 * @note Assumes the trigger is of type BOX_TRIGGER.
+	 */
+	PLUGIN_API void physics_trigger_set_half_extents(void *trigger, float *halfExtents);
+	
+	/**
+	 * Gets the extent of the physics trigger box shape.
+	 * @param [IN]  trigger The physics trigger instance pointer.
+	 * @param [OUT] halfExtentsn the size of the box's extents (3 floats).
+	 * @note Assumes the trigger is of type BOX_TRIGGER.
+	 */
+	PLUGIN_API void physics_trigger_get_half_extents(void *trigger, float *halfExtents);
+	
+	/**
+	 * Sets the radius of the physics trigger sphere shape.
+	 * @param [IN] trigger The physics trigger instance pointer.
+	 * @param [IN] radius The radius of the sphere collider.
+	 * @note Assumes the trigger is of type SPHERE_TRIGGER.
+	 */
+	PLUGIN_API void physics_trigger_set_radius(void *trigger, float radius);
+	
+	/**
+	 * Gets the radius of the physics trigger sphere shape.
+	 * @param [IN] trigger The physics trigger instance pointer.
+	 * @return the radius of the trigger.
+	 * @note Assumes the trigger is of type SPHERE_TRIGGER.
+	 */
+	PLUGIN_API float physics_trigger_get_radius(void *trigger);
+}
+
+
 #endif // _BULLETPLUGIN_PHYSICTRIGGER_API_H_
