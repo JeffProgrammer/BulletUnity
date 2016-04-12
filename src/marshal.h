000ae7ab8e0378b70b0ac3a080e309b3fed12777
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-04-11 20:59:17 -0400

    docs and a box marshal

--- src/marshal.h
+++ src/marshal.h
@@ -32,13 +32,34 @@ namespace Marshal {
 	}
 
 	/**
-	 * Marshals a 3 length float array to 
-	 *
+	 * Marshals 2 vectors to make a box into a float array.
+	 * @param [IN]  min The min of the box.
+	 * @param [IN]  max The max of hte box.
+	 * @param [OUT] arr The 6 length array, outputteed, preallocated.
+	 */
+	inline void toArray(const btVector3 &min, const btVector3 &max, float *&arr) {
+		arr[0] = min.x();
+		arr[1] = min.y();
+		arr[2] = min.z();
+		arr[3] = max.x();
+		arr[4] = max.y();
+		arr[5] = max.z();
+	}
+
+	/**
+	 * Marshals a 3 length float array to a btVector3.
+	 * @param [IN] arr The 3 length array, preallocated.
+	 * @return The btVector that resulted from the array.
 	 */
 	inline btVector3 toVector(const float *arr) {
 		return btVector3(arr[0], arr[1], arr[2]);
 	}
 
+	/**
+	 * Marshals a 4 length float array to a btQuaternion.
+	 * @param [IN] arr The 4 length array, preallocated.
+	 * @return the btQuaternion that resulted from the array.
+	 */
 	inline btQuaternion toQuat(const float *arr) {
 		return btQuaternion(arr[0], arr[1], arr[2], arr[3]);
 	}
